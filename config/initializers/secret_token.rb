8669baa13ddcc4b14f9f1739575defaf3e0e19eb
Martin Tsai <mtsai@creaxon.com>
2013-12-13 00:13:18 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sitemon::Application.config.secret_key_base = 'b5775871f630f50009a6025d6e9afbdd46bcbcf1a6c520779d1902fb15cff070a8ee9dde2fd304b68a649bb644212c3bb2bfbb6acb19e907e4e74a6a4bbb65cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sitemon::Application.config.session_store :cookie_store, key: '_sitemon_session'
