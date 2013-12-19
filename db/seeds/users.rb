455aec358cbf45a398a31b9b88102d201d0ad929
Martin Tsai <mtsai@creaxon.com>
2013-12-20 03:17:11 +0800

    fix typo

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -2,4 +2,4 @@ user = User.new( email: "mtsai@creaxon.com", password: "bigsecret" )
 user.skip_confirmation!
 user.save
 
-Admin.create( email: "admin@creaxon.com", password: "biglsecret" )
+Admin.create( email: "admin@creaxon.com", password: "bigsecret" )
