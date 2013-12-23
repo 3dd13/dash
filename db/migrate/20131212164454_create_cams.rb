c54881b19854672b15760f05aacb91961d80da76
Martin Tsai <mtsai@creaxon.com>
2013-12-23 13:41:24 +0800

    - generate cams locations from original TD dataset - add name attribute to cams

--- db/migrate/20131212164454_create_cams.rb
+++ db/migrate/20131212164454_create_cams.rb
@@ -1,6 +1,7 @@
 class CreateCams < ActiveRecord::Migration
   def change
     create_table :cams do |t|
+      t.string :name
       t.string :uri
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131221045204) do
+ActiveRecord::Schema.define(version: 20131219194843) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -32,9 +32,7 @@ ActiveRecord::Schema.define(version: 20131221045204) do
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "cams", force: true do |t|
-    t.string   "address"
-    t.decimal  "latitude"
-    t.decimal  "longitude"
+    t.string   "name"
     t.string   "uri"
     t.datetime "created_at"
     t.datetime "updated_at"
