a70ce81fc7876e57462ba8acbbe8eeed70ed8b33
Martin Tsai <mtsai@creaxon.com>
2014-01-04 23:24:42 +0800

    dashboards_controller#test: includes :location when generating markers to reduce db accesses

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -31,7 +31,7 @@ class DashboardsController < ApplicationController
 
     gon.origin = @dashboard.point_a.to_latlng
     gon.destination = @dashboard.point_b.to_latlng
-    gon.markers = Cam.all.map do |c|
+    gon.markers = Cam.includes(:location).all.map do |c|
       c.location.to_latlng.merge( { name: c.name, data: "#{render_to_string partial: 'cam', locals:{cam: c} }" } )
     end
   end
