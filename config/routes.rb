3ace0a4ec911864ece45011ede1e01ec3baf7efa
Martin Tsai <mtsai@creaxon.com>
2014-01-08 08:51:13 +0800

    set root_path to dashboards new

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Sitemon::Application.routes.draw do
     get 'tsms/map' => 'tsms#map'
   end
 
-  root :to => "cams#index"
+  root :to => "dashboards#new"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
